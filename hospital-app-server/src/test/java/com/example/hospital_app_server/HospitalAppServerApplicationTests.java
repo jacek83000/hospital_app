7b4b367628963ecc6a43a66b968fda4537dc4788
jacek83000 <jaceksota@gmail.com>
2024-09-17 16:36:51 +0200

    first commit

---
+++ hospital-app-server/src/test/java/com/example/hospital_app_server/HospitalAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospital_app_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
